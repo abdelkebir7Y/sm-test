6aa96e9f2be20ef47662df8cfceca81a12629994
ouhassou abdelkebir <ouhassou.abdelkebir@yobeen.com>
2023-05-02 20:28:32 +0100

    adding sequelize and connect to database

--- server/bin/run.js
+++ server/bin/run.js
@@ -1,9 +1,23 @@
 const http = require("http");
+const Sequelize = require("sequelize");
 
 const config = require("../config")[process.env.NODE_ENV || "development"];
 
 const log = config.log();
 const app = require("../app")(config);
+
+const connectToDatabase = function () {
+  log.debug(config.db.options);
+  const sequelize = new Sequelize(config.db.options);
+  sequelize
+    .authenticate()
+    .then(log.info("Connection has been established successfully."))
+    .catch((error) => log.error("Unable to connect to the database:", error));
+  return sequelize;
+};
+
+const dbClient = connectToDatabase();
+config.db.client = dbClient;
 
 const server = http.createServer(app);
 
--- server/config/index.js
+++ server/config/index.js
@@ -15,6 +15,18 @@ module.exports = {
     name,
     version,
     serviceTimeout: 30,
+    db: {
+      options: {
+        host: "localhost",
+        port: 5432,
+        database: "sm_test",
+        dialect: "postgres",
+        username: "postgres",
+        password: "password",
+        logging: (msg) => getLogger(name, version, "debug").info(msg),
+      },
+      client: null,
+    },
     log: () => getLogger(name, version, "debug"),
   },
   production: {
